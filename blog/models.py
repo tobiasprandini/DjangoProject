4fe7d416c60ef41b3818f35812cb1dab41a4f462
Tobias Prandini <tobiasprandini@gmail.com>
2025-03-05 17:35:59 -0300

    creating foreign key on author from model Post

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
     body = models.TextField()
 
     def __str__(self):
